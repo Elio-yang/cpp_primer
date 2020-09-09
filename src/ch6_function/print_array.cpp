13de12b95a5dc2925722358c357170c035e16478
Elio-yang <2925539854@qq.com>
2020-09-09 15:20:36 +0800

    config

--- src/ch6_function/print_array.cpp
+++ src/ch6_function/print_array.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <iterator>
+#include <vector>
+
 using namespace std;
 #define SIZE 7
 void print_by_indentical(const int * a);
 void print_by_lib(const int * begin,const int * end);
 void print_by_arg(const int * a,size_t size);
 void print_by_ref(int (&arr)[SIZE]);
+void print_by_recursion(vector<int>::iterator it,vector<int> arr);
+
 int main(int argc, char const *argv[])
 {
         int a[SIZE]={1,2,3,4,5,6,0};
+        vector<int> arr={4,5,6,0};
         print_by_indentical(a);
         print_by_lib(begin(a),end(a));
         print_by_arg(a,SIZE);
         print_by_ref(a);
+        print_by_recursion(arr.begin(),arr);
         return 0;
 }
 void print_by_indentical(const int * a)
@@ -45,4 +51,12 @@ void print_by_ref(int (&arr)[SIZE])
                 cout<<i<<" ";
         }
         cout<<endl;
+}
+void print_by_recursion(vector<int>::iterator it,vector<int> arr)
+{
+        if(it==arr.end()){
+                return;
+        }
+        cout<<*it<<" ";
+        return print_by_recursion(++it,arr);
 }
